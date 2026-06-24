b64ce43dec3c9b917a152002f30f2a842fc8c5b9
Trai Lynne <trailynnecloud@gmail.com>
2026-06-24 15:43:57 -0600

    Final cleaning pass: Header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
-import Logo from "../../../public/images/logo.png"
+import Logo from "@/public/images/logo.png"
 import styles from "./header.module.css"
 
 export default function Header() {
   return (
-    <header className={styles.headerStyle}>
-      <Link href={"/"} className={styles.linkStyle}>
-        <Image src={Logo} alt={"Trai Lynne Logo"} className={styles.imgStyle} />
+    <header className={styles.header}>
+      <Link href={"/"} className={styles.link}>
+        <Image src={Logo} alt={"Trai Lynne Logo"} className={styles.img} />
       </Link>
     </header>
   )
