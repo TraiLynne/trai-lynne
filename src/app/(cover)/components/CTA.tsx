5ec3724be5455386d2476f99fcb72d8ad54c01eb
Trai Lynne <trailynnecloud@gmail.com>
2026-06-24 15:04:53 -0600

    Final cleaning pass: Hero CTA import update for consistency

--- src/app/(cover)/components/CTA.tsx
+++ src/app/(cover)/components/CTA.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import styles from "../styles/cta.module.css";
+import styles from "@/src/app/(cover)/styles/cta.module.css";
 
 export default function CTA() {
   return (
--- src/app/(cover)/components/Hero.tsx
+++ src/app/(cover)/components/Hero.tsx
@@ -1,12 +1,17 @@
 import Image from "next/image";
-import Logo from "../../../../public/images/logo.png";
-import styles from "../styles/hero.module.css"
+import Logo from "@/public/images/logo.png"
+import styles from "@/src/app/(cover)/styles/hero.module.css"
 
 export default function Hero() {
     return (
-        <div className={styles.heroContainer}>
-            <div className={styles.logoWrapper}>
-                <Image fill src={Logo} alt="Logo" style={{objectFit: "contain"}} />
+        <div className={styles.container}>
+            <div className={styles.wrapper}>
+                <Image
+                    fill
+                    src={Logo}
+                    alt="Logo"
+                    className={styles.img}
+                />
             </div>
         </div>
     )
