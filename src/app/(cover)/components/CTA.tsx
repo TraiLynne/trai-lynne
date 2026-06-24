d2cc926e7d8538a92c9d37423a443a73edaf86e8
Trai Lynne <trailynnecloud@gmail.com>
2026-06-24 14:50:15 -0600

    Cleaned up CTA component

--- src/app/(cover)/components/CTA.tsx
+++ src/app/(cover)/components/CTA.tsx
@@ -1,10 +1,9 @@
 import Link from "next/link";
-import { CSSProperties } from "react";
 import styles from '../styles/cta.module.css'
 
 export default function CTA() {
   return (
-    <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+    <div className={styles.containerStyle}>
       <Link href={"/about"} className={styles.enterButton}>
         Open
       </Link>
