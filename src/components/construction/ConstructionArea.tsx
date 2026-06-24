bb239eaac46215818901d3d2b8a87f5df6abcf69
Trai Lynne <trailynnecloud@gmail.com>
2026-06-24 15:37:42 -0600

    Final cleaning pass: ConstructionArea

--- src/components/construction/ConstructionArea.tsx
+++ src/components/construction/ConstructionArea.tsx
@@ -3,7 +3,7 @@ import styles from "./construction.module.css"
 export default function ConstructionArea() {
   return (<div className={styles.constructionArea}>
       <div className={styles.constructionTape}>
-        <p className={styles.text}>Under Construction</p>
+        <p className={styles.tapeText}>Under Construction</p>
       </div>
     </div>
   );
