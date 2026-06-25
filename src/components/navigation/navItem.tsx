1460586e4524b0bb06914316c3822ad2f86154b9
Trai Lynne <trailynnecloud@gmail.com>
2026-06-25 14:26:23 -0600

    Create NavItem Component with basic styling

---
+++ src/components/navigation/navItem.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Link from "next/link";
+import useActiveRoute from "./useActiveRoute";
+import styles from "./nav.module.css"
+
+export default function NavItem({href, label}: {href: string, label: string}) {
+  const { isActive } = useActiveRoute();
+
+  const active = isActive(href);
+
+  return (
+    <Link href={href} className={`${styles.item} ${active ? styles.active : styles.inactive}`}>
+      {label}
+    </Link>
+  )
+}
