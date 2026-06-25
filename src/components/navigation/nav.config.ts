348b07260b84d4cfe6b42df92a0b30e1c2596872
Trai Lynne <trailynnecloud@gmail.com>
2026-06-25 14:00:05 -0600

    Create the Navigation Config

---
+++ src/components/navigation/nav.config.ts
@@ -0,0 +1,8 @@
+export const navItems = [
+  { label: "Cover", href: "/" },
+  { label: "About", href: "/about" },
+  { label: "Projects", href: "/projects" },
+  { label: "Resume", href: "/resume" },
+  { label: "Blog", href: "/blog" },
+  { label: "Reference", href: "/ref" },
+];
