3a3da1f39d42b099b0579f8ff1fbba065494c9d0
Trai Lynne <trailynnecloud@gmail.com>
2026-06-25 15:19:08 -0600

    Update Mobile Bookmark Nav Structure Show/Hide based on state

--- src/components/navigation/mobileNav.tsx
+++ src/components/navigation/mobileNav.tsx
@@ -1,12 +1,21 @@
+"use client";
+import { useState } from "react";
 import { navItems } from "./nav.config";
 import NavItem from "./navItem";
+import styles from "./mobileNav.module.css"
 
 export default function MobileNav() {
+  const [isOpen, setIsOpen] = useState(true);
   return (
-    <div>
-      {
-        navItems.map(data => <NavItem key={data.href} {...data} />)
-      }
+    <div className={styles.container}>
+      <button className={styles.button} onClick={() => setIsOpen(!isOpen)}>
+        ☰
+      </button>
+      <div className={`${styles.wrapper} ${isOpen ? styles.activeWrapper : styles.inactiveWrapper}`}>
+        {
+          isOpen && (navItems.map(data => <NavItem key={data.href} {...data} />))
+        }
+      </div>
     </div>
   )
 }
--- src/components/navigation/navItem.tsx
+++ src/components/navigation/navItem.tsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 import useActiveRoute from "./useActiveRoute";
 import styles from "./nav.module.css"
 
-export default function NavItem({href, label}: {href: string, label: string}) {
+export default function NavItem({ href, label }: { href: string, label: string }) {
   const { isActive } = useActiveRoute();
 
   const active = isActive(href);
 
   return (
-    <Link href={href} className={`${styles.item} ${active ? styles.active : styles.inactive}`}>
+    <Link href={href} className={`${styles.item} ${active ? styles.activeItem : styles.inactiveItem}`}>
       {label}
     </Link>
   )
