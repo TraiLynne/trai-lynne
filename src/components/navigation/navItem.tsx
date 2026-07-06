f91da51b58fe79e2b5fa63dcb84532b9e80d9543
Trai Lynne Compton <trailynnecloud@gmail.com>
2026-07-06 14:13:47 -0600

    Tuning desktop styles

--- src/components/navigation/navItem.tsx
+++ src/components/navigation/navItem.tsx
@@ -1,21 +1,40 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
+import desktopStyles from "./desktopNav.module.css";
+import mobileStyles from "./mobileNav.module.css";
 import useActiveRoute from "./useActiveRoute";
-import mobileStyles from "./mobileNav.module.css"
-import desktopStyles from "./desktopNav.module.css"
 
 type NavVariant = "desktop" | "mobile";
 
-export default function NavItem({ href, label, variant }: { href: string, label: string, variant: NavVariant }) {
+export default function NavItem({
+  href,
+  label,
+  variant,
+}: {
+  href: string;
+  label: string;
+  variant: NavVariant;
+}) {
   const { isActive } = useActiveRoute();
   const styles = variant === "mobile" ? mobileStyles : desktopStyles;
 
   const active = isActive(href);
 
   return (
-    <Link href={href} className={`${styles.item} ${active ? styles.activeItem : styles.inactiveItem}`}>
-      {label}
+    <Link
+      href={href}
+      className={`${styles.item} ${active ? styles.activeItem : styles.inactiveItem}`}
+    >
+      <Image
+        className={active ? styles.annotation : styles.hideAnnotation}
+        src="/images/annotations/red-oval.png"
+        alt=""
+        width={200}
+        height={50}
+      />
+      <span>{label}</span>
     </Link>
-  )
+  );
 }
